e1d0f0162739dd1309d2180c0ecae8e23ca1acff
袁志健 <jswxyzj@qq.com>
2025-01-22 18:16:03 +0800

    Add core configurations and utility classes for Jackson and Web MVC
    - Introduced `JacksonConfiguration` for custom ObjectMapper settings, including date formats and locale.
    - Added `WebMvcConfiguration` to handle date-time formatters and internationalization support.
    - Created `CommonConstants` interface for shared constant values.
    - Implemented `JavaTimeModule` for Java 8 time serialization and deserialization.
    - Developed `R` class as a standardized response structure for API results.

---
+++ service-common/common-core/src/main/java/com/jxmk/common/core/constant/CommonConstants.java
@@ -0,0 +1,19 @@
+package com.jxmk.common.core.constant;
+
+/**
+ * 公共常量
+ */
+public interface CommonConstants {
+
+	/**
+	 * 成功标记
+	 */
+	Integer SUCCESS = 0;
+
+	/**
+	 * 失败标记
+	 */
+	Integer FAIL = 1;
+
+
+}
